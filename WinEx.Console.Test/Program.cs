d4c14ace77e8a1cbdb9ebc4cdb69cfb6e1974150
design0er <design0er@gmail.com>
2018-01-02 00:32:14 +0800

    first commit,hook test

---
+++ WinEx.Console.Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinEx.Console.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("program start.press any key to start:");
+            System.Console.ReadKey();
+            InterceptKeys.Run();
+            System.Console.WriteLine("press the key to end: Ctrl+c");
+            while (true)
+            {
+                var key = System.Console.ReadKey().Key;
+                if (key == ConsoleKey.E)
+                {
+                    InterceptKeys.Disable();
+                    System.Console.WriteLine("app exit.");
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
